f3ba125c3a9fedd56b0360389b02729e40eae7ac
Whiz75 <75149500+Whiz75@users.noreply.github.com>
2021-05-24 18:07:49 +0200

    car wash app

---
+++ app/src/main/java/com/example/findcarwashapp/dialogs/ProfileDialogFragment.java
@@ -0,0 +1,47 @@
+package com.example.findcarwashapp.dialogs;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.findcarwashapp.R;
+
+import java.util.Objects;
+
+
+public class ProfileDialogFragment extends DialogFragment {
+
+    public ProfileDialogFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        //set the dialog width and height
+        Objects.requireNonNull(getDialog())
+                .getWindow()
+                .setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup view = (ViewGroup)inflater.inflate(R.layout.fragment_profile_dialog, container, false);
+
+        return view;
+    }
+}
